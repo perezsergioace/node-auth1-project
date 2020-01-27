e25fc51486a3f30cf6edd62f2f793cc322a0ba45
perezsergioace <perez.sergio.ace@gmail.com>
2020-01-27 17:32:46 -0500

    Setup the knexfile and the dbconfif file

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,21 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/users.db3',
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
   },
 
   staging: {
