397fbb552ea40793f0178f550299264f966ddc04
perezsergioace <perez.sergio.ace@gmail.com>
2020-01-27 17:53:18 -0500

    fixed typo in the users seed file

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -4,7 +4,7 @@ exports.seed = function(knex) {
   return knex('users').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('usersx').insert([
+      return knex('users').insert([
         {username: 'user 1', password: 'password1'},
         {username: 'user 2', password: 'password2'},
         {username: 'user 3', password: 'password3'},
